1f11c53c0727e1b30d2012e613d86782b67d2921
dimmmark <105634628+dimmmark@users.noreply.github.com>
2022-12-11 14:35:53 +0400

    Add shooting machanics

--- Assets/Scripts/AimTurret.cs
+++ Assets/Scripts/AimTurret.cs
@@ -12,6 +12,6 @@ public class AimTurret : MonoBehaviour
         var desiredAngle = Mathf.Atan2(turretDirection.y, turretDirection.x) * Mathf.Rad2Deg;
         var rotationStep = turretRotationSpeed * Time.deltaTime;
         transform.rotation = Quaternion.RotateTowards(transform.rotation,
-            Quaternion.Euler(0, 0, desiredAngle - 90), rotationStep);
+            Quaternion.Euler(0, 0, desiredAngle ), rotationStep);
     }
 }
